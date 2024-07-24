20d9eef136be55d8b98bb6ec22b3910872068c8d
Akash Shaw <shawakash2003@gmail.com>
2024-07-24 13:48:41 +0530

    chore: use the market maker module

--- crates/market_maker/src/main.rs
+++ crates/market_maker/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod api_client;
+mod mm;
+
+use api_client::ApiClient;
+use mm::MarketMaker;
+
+#[tokio::main]
+async fn main() {
+    let api_client = ApiClient::new("https://api.backpack.exchange/api/v1");
+    let market_maker = MarketMaker::new(api_client, 0.001);
+
+    market_maker.run("SOL_USDC").await;
 }
