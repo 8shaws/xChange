47e7e93497725ba40e97126488ad016c6039193b
Akash Shaw <shawakash2003@gmail.com>
2024-07-25 19:08:07 +0530

    chore: adds /user/register router to register the user simply

--- crates/api/src/models.rs
+++ crates/api/src/models.rs
@@ -1,10 +1,11 @@
 use chrono::NaiveDateTime;
 use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::schema::users;
 
-#[derive(Queryable, Selectable)]
+#[derive(Queryable, Selectable, Insertable, Serialize)]
 #[diesel(table_name = users)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
@@ -17,4 +18,14 @@ pub struct User {
     pub contact_number: String,
     pub created_at: NaiveDateTime,
     pub updated_at: NaiveDateTime,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RegisterUser {
+    pub first_name: String,
+    pub last_name: String,
+    pub username: String,
+    pub password: String,
+    pub email: String,
+    pub contact_number: String,
 }
