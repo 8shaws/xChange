9d46932a9c6ae04205b94a4b7e23d13a210bf04a
Akash Shaw <shawakash2003@gmail.com>
2024-07-25 11:23:04 +0530

    chore: adds fn to establish connectiona and schema for db

---
+++ crates/api/src/schema.rs
@@ -0,0 +1,21 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    users (id) {
+        id -> Uuid,
+        #[max_length = 255]
+        username -> Varchar,
+        #[max_length = 255]
+        password -> Varchar,
+        #[max_length = 255]
+        first_name -> Varchar,
+        #[max_length = 255]
+        last_name -> Varchar,
+        #[max_length = 255]
+        email -> Varchar,
+        #[max_length = 255]
+        contact_number -> Varchar,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
