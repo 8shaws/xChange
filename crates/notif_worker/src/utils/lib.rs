418b8903a5f310adac1a7fceacf5fef6a1f679f6
Akash Shaw <shawakash2003@gmail.com>
2024-07-29 22:36:11 +0530

    chore: adds fn to generate otp

--- crates/notif_worker/src/utils/lib.rs
+++ crates/notif_worker/src/utils/lib.rs
@@ -1,11 +1,14 @@
-use lettre::message::{header, Message};
+use lettre::message::{header, Message, MultiPart, SinglePart};
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{SmtpTransport, Transport};
 use std::env;
+use totp_rs::{Algorithm, TOTP};
+
+use crate::constants::get_email_html;
 
 use super::current_time;
 
-pub async fn send_mail(mail: &str, thread_id: &str) {
+pub async fn send_mail(mail: &str, otp: &str, thread_id: &str) {
     let smtp_username = env::var("SMTP_USERNAME").expect("SMTP_USERNAME must be set");
     let smtp_pass = env::var("SMTP_PASS").expect("SMTP_PASS must be set");
     let reply_to = env::var("REPLY_TO_MAIL").expect("REPLY_TO_MAIL must be set");
@@ -14,11 +17,14 @@ pub async fn send_mail(mail: &str, thread_id: &str) {
         .from(smtp_username.parse().unwrap())
         .reply_to(reply_to.parse().unwrap())
         .to(mail.parse().unwrap())
-        .subject("Hello from xChange!")
-        .header(header::ContentType::TEXT_PLAIN)
-        .body(String::from(
-            "This is a test email from xChange using lettre.",
-        ))
+        .subject("[verify]: Email verification otp from xChange!")
+        .multipart(
+            MultiPart::alternative().singlepart(
+                SinglePart::builder()
+                    .header(header::ContentType::TEXT_HTML)
+                    .body(get_email_html(otp)),
+            ),
+        )
         .unwrap();
 
     let creds = Credentials::new(smtp_username, smtp_pass);
@@ -30,10 +36,32 @@ pub async fn send_mail(mail: &str, thread_id: &str) {
 
     match mailer.send(&email) {
         Ok(_) => println!(
-            "{}: Worker {} Email sent successfully!",
+            "{}: Worker {}: Email sent successfully!",
             current_time(),
             thread_id
         ),
         Err(e) => println!("Could not send email: {:?}", e),
     }
+}
+
+pub fn generate_otp() -> String {
+    let otp_secret = env::var("OTP_SECRET").expect("OTP_SECRET must be set");
+    let totp = TOTP::new(
+        Algorithm::SHA1,
+        6,
+        1,
+        2 * 60,
+        otp_secret.as_bytes().to_vec(),
+    )
+    .unwrap();
+
+    let otp = totp.generate_current().unwrap();
+    otp
+}
+
+pub fn parse_json<T>(data: &str) -> T
+where
+    T: serde::de::DeserializeOwned,
+{
+    serde_json::from_str(data).unwrap()
 }
