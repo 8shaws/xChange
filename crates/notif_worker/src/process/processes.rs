b903a86657940ed21b25a4e4c44bde441acc5fb8
Akash Shaw <shawakash2003@gmail.com>
2024-07-29 22:37:09 +0530

    chore: sets the otp to redis and sends mail

--- crates/notif_worker/src/process/processes.rs
+++ crates/notif_worker/src/process/processes.rs
@@ -1,8 +1,11 @@
 use r2d2_redis::redis;
+use serde_json;
+use serde_json::Value;
 use std::thread;
 use tokio::time::{sleep, Duration};
 
 use crate::utils::current_time;
+use crate::utils::lib::generate_otp;
 use crate::utils::lib::send_mail;
 
 pub async fn send_email_process(conn: &mut redis::Connection) {
@@ -12,15 +15,44 @@ pub async fn send_email_process(conn: &mut redis::Connection) {
     let result = redis::cmd("RPOP").arg(queue).query::<Option<String>>(conn);
 
     match result {
-        Ok(Some(mail)) => {
+        Ok(Some(res)) => {
+            let data: Value = serde_json::from_str(&res).unwrap();
             println!(
-                "{}: Worker {} processing item from {}: {}",
+                "{}: Worker {}: processing item from {}: {}",
                 current_time(),
                 thread_id,
                 queue,
-                mail
+                data["mail"]
             );
-            send_mail(&mail, &thread_id).await;
+
+            let otp = generate_otp();
+
+            let catch_otp_result = redis::cmd("SET")
+                .arg(format!("otp:{}", data["id"]))
+                .arg(&otp)
+                .query::<Option<String>>(conn);
+
+            match catch_otp_result {
+                Ok(_) => {
+                    println!(
+                        "{}: Worker {}: OTP for {} saved successfully!",
+                        current_time(),
+                        thread_id,
+                        data["mail"]
+                    );
+                    send_mail(&data["mail"].as_str().unwrap(), &otp, &thread_id).await;
+                }
+                Err(err) => {
+                    println!(
+                        "{}: Worker {}: Failed to save OTP for {}: {}",
+                        current_time(),
+                        thread_id,
+                        data["mail"],
+                        err
+                    );
+                }
+            }
+
             sleep(Duration::from_secs(1)).await;
         }
         Ok(None) => {
