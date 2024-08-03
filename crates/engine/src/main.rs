8543fe34339485861ed7246942881b5e8908e336
Akash Shaw <shawakash2003@gmail.com>
2024-08-03 22:35:41 +0530

    chore: creating infinite running fn

--- crates/engine/src/main.rs
+++ crates/engine/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+use std::sync::Arc;
+use tokio;
+use tokio::signal;
+
+mod process;
+
+use common::redis::initialize_redis_pool;
+use process::handle_process;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let no_worker_threads = std::env::var("NO_WORKER_THREADS")
+        .unwrap_or_else(|_| "5".to_string())
+        .parse::<usize>()
+        .expect("NO_WORKER_THREADS must be a positive integer");
+
+    let queues = vec!["user_email_verify"];
+
+    let pool = Arc::new(initialize_redis_pool());
+
+    let mut worker_handlers = vec![];
+
+    for _ in 0..no_worker_threads {
+        let pool_clone = Arc::clone(&pool);
+        let queues_clone = queues.clone();
+        let worker_handle = tokio::spawn(async move {
+            handle_process(pool_clone, queues_clone).await;
+        });
+        worker_handlers.push(worker_handle);
+    }
+
+    let shutdown_signal = async {
+        signal::ctrl_c().await.expect("Failed to listen for ctrl+c");
+        println!(
+            "{}: Shutdown signal received",
+            common::utils::current_time()
+        );
+    };
+
+    shutdown_signal.await;
+
+    for handle in worker_handlers {
+        handle.abort();
+    }
+    println!("{}: Shutting down...", common::utils::current_time());
 }
