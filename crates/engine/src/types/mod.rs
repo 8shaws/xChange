1d3c11f541323f1a29ab2fedc719f166a8d8c74b
Akash Shaw <shawakash2003@gmail.com>
2024-08-15 01:02:43 +0530

    chore: some types change

--- crates/engine/src/types/mod.rs
+++ crates/engine/src/types/mod.rs
@@ -1,5 +1,5 @@
 use crate::trade::{engine::Balances, orderbook::Orderbook};
-use common::types::order::Fill;
+use common::types::order::{Fill, Order};
 use serde::{Deserialize, Serialize};
 
 pub struct AddOrderResponse {
@@ -11,4 +11,26 @@ pub struct AddOrderResponse {
 pub struct SnapShot {
     pub orderbooks: Vec<Orderbook>,
     pub balances: Vec<Balances>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum MessageType {
+    AddOrder,
+    CancelOrder,
+    UpdateOrder,
+    ProcessOrder,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Message {
+    #[serde(rename = "type")]
+    pub type_: MessageType,
+    pub data: Order,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Process {
+    pub message: Message,
+    pub client_id: String,
 }
