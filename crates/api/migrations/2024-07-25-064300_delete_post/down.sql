cec0fc31840a15684ca5e2158e8f37d332be34fc
Akash Shaw <shawakash2003@gmail.com>
2024-08-17 19:50:49 +0530

    chore: rm migrations from api

--- crates/api/migrations/2024-07-25-064300_delete_post/down.sql
+++
@@ -1,9 +0,0 @@
--- This file should undo anything in `up.sql`
-CREATE TABLE "posts"(
-	"id" INT4 NOT NULL PRIMARY KEY,
-	"title" VARCHAR NOT NULL,
-	"body" TEXT NOT NULL,
-	"published" BOOL NOT NULL
-);
-
-
--- crates/api/migrations/2024-07-27-100150_hash_pass/up.sql
+++
@@ -1,4 +0,0 @@
--- Your SQL goes here
-ALTER TABLE "users" DROP COLUMN "password";
-ALTER TABLE "users" ADD COLUMN "hash_password" VARCHAR(255) NOT NULL;
-
