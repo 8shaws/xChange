c3a3d32e1297a0292b8895d25720dde2322bc67e
Akash Shaw <shawakash2003@gmail.com>
2024-08-27 19:53:49 +0530

    chore: adds kyc to models

--- crates/db/src/models.rs
+++ crates/db/src/models.rs
@@ -1,9 +1,17 @@
 use chrono::NaiveDateTime;
+use diesel::deserialize::FromSqlRow;
+use diesel::expression::AsExpression;
 use diesel::{prelude::*, query_builder::QueryId};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+use crate::schema::kyc;
 use crate::schema::users;
+
+use diesel::deserialize::{self, FromSql};
+use diesel::pg::Pg;
+use diesel::serialize::{self, IsNull, Output, ToSql};
+use std::io::Write;
 
 #[derive(Queryable, Insertable, Serialize, QueryId, Debug, Selectable, Deserialize, Clone)]
 #[diesel(table_name = users)]
@@ -31,8 +39,24 @@ pub struct RegisterUser {
     pub contact_number: String,
 }
 
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-// pub enum LoginField {
-//     Email(String),
-//     ContactNumber(String),
-// }
+#[derive(Queryable, Insertable, Serialize, QueryId, Debug, Selectable, Deserialize, Clone)]
+#[diesel(table_name = kyc)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Kyc {
+    pub id: Uuid,
+    pub user_id: Uuid,
+    pub document_type: String,
+    pub document_number: String,
+    pub issue_country: String,
+    pub expiry_date: NaiveDateTime,
+    pub document_front_url: String,
+    pub document_back_url: String,
+    pub selfie_url: String,
+    pub verification_status: String,
+    pub submitted_at: NaiveDateTime,
+    pub verified_at: Option<NaiveDateTime>,
+    pub rejected_at: Option<NaiveDateTime>,
+    pub rejection_reason: Option<String>,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
